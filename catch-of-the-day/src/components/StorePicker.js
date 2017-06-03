6d193f1e454c75c6f5c96e62dbd6041f7347b861
Jason Lai <jase_lai@hotmail.com>
2017-06-03 19:50:10 +0100

    Video 7 completed

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+      {/* This is a comment*/}
+        <h2>Please Enter a Store</h2>
+        <input type="text" placeholder="Store Name" required/>
+        <button type="submit">Visit Store -></button>
+      </form>
+      ) 
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,12 @@
 // let's go!
+
+//libraries
+import React from 'react';
+import {render} from 'react-dom';
+import './css/style.css'
+
+//components
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+render(<App/>, document.querySelector('#main')); 
